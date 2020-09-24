ce96befc8c59c72d06440e9353d963b23a1ebc3a
Deku <DerDeku@gmail.com>
2020-09-24 22:49:39 +0200

    Map1 0.1
    Terrain added

---
+++ Weekend-Project/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float speed = .1f;
+    private void Update()
+    {
+        float dirX = Input.GetAxis("Horizontal");
+        float dirZ = Input.GetAxis("Vertical");
+
+        Vector3 moveDir = new Vector3(dirX, 0, dirZ);
+
+        transform.position += moveDir * speed;
+    }
+}
